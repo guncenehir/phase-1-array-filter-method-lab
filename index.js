1d5d7232931e137a781c2b31e9afe68f938abc69
Nehir Gencay <nhrgncy@gmail.com>
2023-04-28 16:24:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, query){
+    return drivers.filter(driver => 
+        driver.toLowerCase()=== query.toLowerCase()
+        );
+}
+
+function fuzzyMatch(drivers, testString){
+    return drivers.filter(driver => 
+        driver.toLowerCase().startsWith(testString.toLowerCase())
+        );
+}
+
+function matchName(drivers, testString){
+    return drivers.filter(driver =>
+        driver.name.toLowerCase()=== testString.toLowerCase()
+        );
+}
